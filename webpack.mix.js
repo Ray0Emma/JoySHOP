753d86a0e108cc1ebc32e94c3ed519e84fdedd4e
Ray0Emma <ahmadifarah22@gmail.com>
2021-08-27 08:51:27 +0000

    adding Procfile

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,8 +14,4 @@ const mix = require('laravel-mix');
 mix.copyDirectory('resources/backend', 'public/backend');
 mix.copyDirectory('resources/frontend', 'public/frontend');
 
-// mix.js('resources/js/app.js', 'public/js')
-//     .vue()
-//     .sass('resources/sass/app.scss', 'public/css');
-
 mix.js('resources/js/app.js', 'public/backend/js').vue();
